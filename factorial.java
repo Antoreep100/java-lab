9b15b91fd5b6e8c2ad895ad78f91e56fb2ae3031
Md. Antoreep Bin Bayzid <afnantheviper@gmail.com>
2025-01-29 12:21:53 +0600

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,18 @@
+public class Main
+{
+	public static void main(String[] args)
+	{
+
+	int number=5;
+	long factorial =1;
+	
+
+	for(int i=1;i<=5;i++)
+	{
+	    factorial *= i;
+	   System.out.print("The factorial of 5 is:" + factorial);
+	}
+	
+
+	}
+}
