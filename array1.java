da037f23f39f2e245ec895c16f20d1b3d95eee1a
Md. Antoreep Bin Bayzid <afnantheviper@gmail.com>
2025-04-16 12:32:41 +0600

    Create array1.java

---
+++ array1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+public static void main (String[] args) 
+{
+  char[] chars = {'a','c','g','x','y','z'};
+  System.out.println{"Index of letter t is " + Arrays.binarySearch(chars, 't');
+    
+  }
+}
+
+
