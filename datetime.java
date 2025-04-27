59f2f2090d6e4430a4c4896550b12878377b61a0
Md. Antoreep Bin Bayzid <afnantheviper@gmail.com>
2025-04-27 22:54:51 +0600

    Create datetime.java

---
+++ datetime.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class CurrentDateTimeBasic {
+    public static void main(String[] args) {
+        // Create a Date object
+        Date date = new Date();
+
+        // Print the date directly
+        System.out.println("Current Date and Time: " + date);
+    }
+}
