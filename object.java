61902222731e34ca930b1ee8c01eead27650b31b
Md. Antoreep Bin Bayzid <afnantheviper@gmail.com>
2025-04-29 11:04:34 +0600

    Create object.java

---
+++ object.java
@@ -0,0 +1,14 @@
+class Main {
+    int a ;
+    boolean b;
+     
+    
+    
+    public static void main(String[] args) {
+        
+       Main obj= new Main();
+       System.out.println("Default value:");
+        System.out.println("a: " + obj.a);
+         System.out.println("b: " + obj.b);
+    }
+}
